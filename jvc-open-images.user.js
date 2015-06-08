05c06143ec08efc8c5752e8bc472566116096eb4
Valérian Galliat <val@codejam.info>
2015-06-08 18:33:57 +0200

    Comply to JavaScript Standard Style
    Link: https://github.com/feross/standard

--- jvc-open-images.user.js
+++ jvc-open-images.user.js
@@ -6,58 +6,55 @@
 // @version 1.0.0
 // ==/UserScript==
 
-var links     = $('.conteneur-messages-pagi .bloc-message-forum .conteneur-message .bloc-contenu .txt-msg p a');
-var header    = document.querySelectorAll('.bloc-pre-pagi-forum')[0];
-var openImage = document.createElement('a');
-var openLink  = document.createElement('a');
+var links = document.querySelectorAll('.conteneur-messages-pagi .bloc-message-forum .conteneur-message .bloc-contenu .txt-msg p a')
+var header = document.querySelectorAll('.bloc-pre-pagi-forum')[0]
+var openImage = document.createElement('a')
+var openLink = document.createElement('a')
 
-openImage.innerHTML = "Ouvrir toutes les images";
-openImage.href      = "#";
-openImage.onclick   = openImages;
+openImage.innerHTML = 'Ouvrir toutes les images'
+openImage.href = '#'
+openImage.onclick = openImages
 
-header.style.overflow = "hidden";
+header.style.overflow = 'hidden'
 
-openImage.style.marginTop    = "5px";
-openImage.style.marginBottom = "5px";
-openImage.style.display      = "block";
-openImage.style.width        = "200px";
-openImage.style.cssFloat     = "left"
+openImage.style.marginTop = '5px'
+openImage.style.marginBottom = '5px'
+openImage.style.display = 'block'
+openImage.style.width = '200px'
+openImage.style.cssFloat = 'left'
 
-openLink.innerHTML = "Ouvrir tous les liens";
-openLink.href      = "#";
-openLink.onclick   = openLinks;
+openLink.innerHTML = 'Ouvrir tous les liens'
+openLink.href = '#'
+openLink.onclick = openLinks
 
-openLink.style.marginTop    = "5px";
-openLink.style.marginBottom = "5px";
-openLink.style.display      = "block";
-openLink.style.cssFloat     = "right"
+openLink.style.marginTop = '5px'
+openLink.style.marginBottom = '5px'
+openLink.style.display = 'block'
+openLink.style.cssFloat = 'right'
 
-header.appendChild(openImage);
-header.appendChild(openLink);
+header.appendChild(openImage)
+header.appendChild(openLink)
 
-var imagesExt = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+var imagesExt = ['jpg', 'jpeg', 'png', 'gif', 'bmp']
 
-function openImages()
-{
-    for (var i = 0; i < links.length; i++) {
-        var linkExp   = links[i].href.split('.');
-        var extension = linkExp[linkExp.length - 1];
-        var inArray   = imagesExt.indexOf(extension);
-        if (inArray !== -1) {
-            open(links[i].href);
-        }
+function openImages () {
+  for (var i = 0; i < links.length; i++) {
+    var linkExp = links[i].href.split('.')
+    var extension = linkExp[linkExp.length - 1]
+    var inArray = imagesExt.indexOf(extension)
+    if (inArray !== -1) {
+      window.open(links[i].href)
     }
+  }
 }
 
-function openLinks()
-{
-    for (var i = 0; i < links.length; i++) {
-        var linkExp   = links[i].href.split('.');
-        var extension = linkExp[linkExp.length - 1];
-        var inArray = imagesExt.indexOf(extension);
-        if (inArray === -1) {
-            open(links[i].href);
-        }
+function openLinks () {
+  for (var i = 0; i < links.length; i++) {
+    var linkExp = links[i].href.split('.')
+    var extension = linkExp[linkExp.length - 1]
+    var inArray = imagesExt.indexOf(extension)
+    if (inArray === -1) {
+      window.open(links[i].href)
     }
+  }
 }
-
